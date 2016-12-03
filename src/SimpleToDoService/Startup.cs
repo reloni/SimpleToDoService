978a4a926c8a49355ffb79230d65870afd93e9c4
Anton Efimenko <reloni@ya.ru>
2016-12-03 18:55:25 +0300

    Init mvc project

---
+++ src/SimpleToDoService/Startup.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace SimpleToDoService
+{
+	public class Startup
+	{
+		public IConfigurationRoot Configuration { get; }
+
+		public Startup(IHostingEnvironment env)
+		{
+			var builder = new ConfigurationBuilder()
+				.SetBasePath(env.ContentRootPath)
+				.AddJsonFile("AppSettings.json", optional: false, reloadOnChange: true)
+				.AddEnvironmentVariables();
+			Configuration = builder.Build();
+		}
+
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddMvc();
+
+			var connectionString = Configuration["DbContextSettings:ConnectionString"];
+			Console.WriteLine(connectionString);
+			//services.AddDbContext<DatabaseContext>(opts => opts.UseNpgsql(connectionString));
+		}
+
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+
+			app.UseMvc();
+			//app.UseMiddleware<HeaderCheckMiddleware>();
+		}
+	}
+}
---
+++ src/SimpleToDoService/ToDoController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleToDoService
+{
+	[Route("api/[controller]")]
+	public class ToDoController : Controller
+	{
+		public JsonResult Get()
+		{
+			return Json(new { Test = "response" });
+		}
+	}
+}
