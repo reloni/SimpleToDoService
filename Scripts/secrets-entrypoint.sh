d9b6ff55926db26b69b1653e6ded75efdba61f07
Anton Efimenko <reloni@ya.ru>
2017-02-06 15:05:43 +0300

    AWS container setup (Part 1)

---
+++ Scripts/secrets-entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "TEST"
+if [ "$LOAD_S3_SECRETS" = "YES" ]; then
+  # Load the S3 secrets file contents into the environment variables
+  eval $(aws s3 --region ${SECRETS_BUCKET_REGION} cp s3://${SECRETS_BUCKET_NAME}/${SECRETS_FILE_NAME} - | sed 's/^/export /')
+fi
+
+dotnet SimpleToDoService.dll
