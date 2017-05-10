12d1d0397d4781ad7ae8087e3c4fa2073ac3ac26
Anton Efimenko <reloni@ya.ru>
2017-05-10 15:29:47 +0300

    Update containers configuration
    Add Release build script and docker file.
    Update net core containers to lates version of net core.

--- Scripts/BuildImage_Debug.sh
+++ Scripts/BuildImage_Debug.sh
@@ -3,9 +3,9 @@ set -e
 
 if [ "${TRAVIS_TAG}" != "" ]; then
 
-  docker run -it -d --name builder microsoft/dotnet:1.1.0-sdk-msbuild tail -f /dev/null
+  docker run -it -d --name builder microsoft/dotnet:1.1.2-sdk tail -f /dev/null
   docker cp src/SimpleToDoService builder:app
-  docker exec builder bash -c 'cd /app; dotnet restore; dotnet publish -o "../published/debug"'
+  docker exec builder bash -c 'cd /app; dotnet restore; dotnet publish --configuration release -o "../published/release"'
   docker cp builder:published published
 
   #push to AWS
---
+++ Scripts/BuildImage_Release.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+set -e
+
+if [ "${TRAVIS_TAG}" != "" ]; then
+
+  docker run -it -d --name builder microsoft/dotnet:1.1.2-sdk tail -f /dev/null
+  docker cp src/SimpleToDoService builder:app
+  docker exec builder bash -c 'cd /app; dotnet restore; dotnet publish --configuration release -o "../published/release"'
+  docker cp builder:published published
+
+  #push to AWS
+  aws ecr get-login --region eu-central-1 > login
+  eval "$(cat login)"
+  export REPO=${DOCKER_AWS_REPONAME}
+  export TAG=${TRAVIS_TAG}-release
+  docker build -f Dockerfile.release -t $REPO:$TAG .
+  docker tag $REPO:$TAG $REPO:latest
+  docker push $REPO > PushLog.log
+  echo "AWS push log ===="
+  cat PushLog.log
+  echo "======"
+
+  #push to docker-hub
+  docker login -u ${DOCKER_USER} -p ${DOCKER_PASS}
+  export REPO=reloni/todo-service
+  docker build -f Dockerfile.release -t $REPO:$TAG .
+  docker tag $REPO:$TAG $REPO:latest
+  docker push $REPO > PushLog.log
+  echo "Docker hub push log ===="
+  cat PushLog.log
+  echo "======"
+fi
+
+exit 0;
