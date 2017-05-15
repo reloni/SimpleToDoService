b86a5e1d13fb024a43a1735e45643fe6bb178aa3
Anton Efimenko <reloni@ya.ru>
2017-05-15 07:31:00 +0300

    Remove TargetDateIncludeTime checks
    Now client should send correct time of push notification

--- src/SimpleToDoService/Common/Common.cs
+++ src/SimpleToDoService/Common/Common.cs
@@ -27,12 +27,7 @@ static class Extensions
 	{
 		if (!task.TargetDate.HasValue)
 			return null;
-
-		var notificationDate = task.TargetDate.Value.ToUniversalTime();
-
-		if (!task.TargetDateIncludeTime)
-			return new DateTime(notificationDate.Year, notificationDate.Month, notificationDate.Day, 0, 0, 0, notificationDate.Kind);
-
+		
 		return task.TargetDate;
 	}
 }
