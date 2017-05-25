947dab19be0f9b4e18c9ade8cf33164beb99cf20
Anton Efimenko <reloni@ya.ru>
2017-05-25 21:39:15 +0300

    Add restore scripts

--- Scripts/RunContainer.sh
+++ Scripts/RunContainer.sh
@@ -2,3 +2,5 @@ docker run -d --name task-manager-service -p 5000:5000 -e POSTGRES_PASSWORD=test
 
 
 docker run -d --name task-manager-service -p 5000:5000 -e POSTGRES_PASSWORD=test -e POSTGRES_USER=taskmanager -e POSTGRES_DB=tasksdb -e POSTGRES_HOST=localhost -e POSTGRES_PORT=5432 test-todo
+
+docker run --name serv -p 5000:5000 -v /Users/AntonEfimenko/pgdata_back:/pgdata_back -e POSTGRES_PASSWORD=psw -e PGDATA_BACKUP=/pgdata_back/latest.psql.gz -e BACKUP_RESTORE_LOG=/pgdata_back/restore.log -e POSTGRES_USER=postgres -e POSTGRES_HOST=localhost -e POSTGRES_PORT=5432 -e POSTGRES_DB=postgres -d serv-restore
--- Scripts/secrets-entrypoint.sh
+++ Scripts/secrets-entrypoint.sh
@@ -4,5 +4,7 @@ if [ "$LOAD_S3_SECRETS" = "YES" ]; then
   # Load the S3 secrets file contents into the environment variables
   eval $(aws s3 --region ${SECRETS_BUCKET_REGION} cp s3://${SECRETS_BUCKET_NAME}/${SECRETS_FILE_NAME} - | sed 's/^/export /')
 fi
+echo "start restore db"
+sh /RestoreDB.sh
 
 dotnet SimpleToDoService.dll
