cbc4317ee489cf8220e41ed00872e68db9098ce3
Anton Efimenko <reloni@ya.ru>
2017-05-31 21:13:23 +0300

    Fix restore db script

--- Scripts/RestoreDB.sh
+++ Scripts/RestoreDB.sh
@@ -4,5 +4,5 @@ if [ -f "${PGDATA_BACKUP}" ]; then
   gunzip -c ${PGDATA_BACKUP} | psql --dbname=postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB} 2> ${BACKUP_RESTORE_LOG} && \
   rm ${PGDATA_BACKUP}
 else
-  cat /CreateDB.sql | psql --dbname=postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}}/${POSTGRES_DB} 2> ${BACKUP_RESTORE_LOG}
+  cat /CreateDB.sql | psql --dbname=postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB} 2> ${BACKUP_RESTORE_LOG}
 fi
