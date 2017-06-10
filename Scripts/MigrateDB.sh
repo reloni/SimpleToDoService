dad7007383e6ea701d9b03bff5b595c568a92b44
Anton Efimenko <reloni@ya.ru>
2017-06-10 22:24:36 +0300

    Update dockerfile

--- Scripts/MigrateDB.sh
+++ Scripts/MigrateDB.sh
@@ -3,7 +3,7 @@
 DBVERSION=$(psql --dbname=postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB} -t -c 'select version from dbversion limit 1;')
 
 if [ "$DBVERSION" -eq "1" ]; then
-	cat ./Migrations/1.sql | psql --dbname=postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB} > ${MIGRATE_LOG} 2>&1
+	cat /Migrations/1.sql | psql --dbname=postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB} > ${MIGRATE_LOG} 2>&1
 elif [ "$DBVERSION" -eq "2" ]; then
 	echo "two"
 else
