2abc4c7c1c2631cc87e78ba95f1522619f94d9d0
Anton Efimenko <reloni@ya.ru>
2017-06-12 21:09:18 +0300

    Modify build script

--- Scripts/Publish.sh
+++ Scripts/Publish.sh
@@ -15,15 +15,27 @@ if [ "${TRAVIS_TAG}" != "" ]; then
 
   docker run -it -d --name builder microsoft/dotnet:1.1.2-sdk tail -f /dev/null
   docker cp src/SimpleToDoService builder:app
-  docker exec builder bash -c 'cd /app; dotnet restore; dotnet publish --configuration release -o "../published/release"'
+  if [ "$SUBTAG" = "release" ]; then
+    docker exec builder bash -c 'cd /app; dotnet restore; dotnet publish --configuration release -o "../published/release"'
+  else
+    docker exec builder bash -c 'cd /app; dotnet restore; dotnet publish --configuration debug -o "../published/debug"'
+  fi
   docker cp builder:published published
+
+  if [ "$SUBTAG" = "release" ]; then
+    export DOCKERFILE=Dockerfile.release
+  else
+    export DOCKERFILE=Dockerfile.debug
+  fi
 
   #push to AWS
   aws ecr get-login --no-include-email --region ${DOCKER_AWS_REGION} > login
   eval "$(cat login)"
-  docker build -f Dockerfile.release -t $REPO:$TAG .
+  docker build -f $DOCKERFILE -t $REPO:$TAG .
   if [ "$SUBTAG" = "release" ]; then
     docker tag $REPO:$TAG $REPO:latest
+  else
+    docker tag $REPO:$TAG $REPO:dev-latest
   fi
   docker push $REPO > PushLog.log
   echo "AWS push log ===="
@@ -33,9 +45,11 @@ if [ "${TRAVIS_TAG}" != "" ]; then
   #push to docker-hub
   docker login -u ${DOCKER_USER} -p ${DOCKER_PASS}
   export REPO=reloni/todo-service
-  docker build -f Dockerfile.release -t $REPO:$TAG .
+  docker build -f $DOCKERFILE -t $REPO:$TAG .
   if [ "$SUBTAG" = "release" ]; then
     docker tag $REPO:$TAG $REPO:latest
+  else
+    docker tag $REPO:$TAG $REPO:dev-latest
   fi
   docker push $REPO > PushLog.log
   echo "Docker hub push log ===="
