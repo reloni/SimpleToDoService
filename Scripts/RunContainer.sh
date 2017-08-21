3f3f15d03cbf09d5da534b00124d8007299906c8
Anton Efimenko <reloni@ya.ru>
2017-08-21 07:06:07 +0300

    Use aspnetcore docker image

--- Scripts/RunContainer.sh
+++ Scripts/RunContainer.sh
@@ -8,3 +8,5 @@ docker build -f Dockerfile.debug -t serv-restore .
 docker run --name serv -p 5000:5000 -v /Users/AntonEfimenko/pgdata_back:/pgdata_back -e POSTGRES_PASSWORD=psw -e PGDATA_BACKUP=/pgdata_back/latest.psql.gz -e BACKUP_RESTORE_LOG=/pgdata_back/restore.log -e POSTGRES_USER=postgres -e POSTGRES_HOST=localhost -e POSTGRES_PORT=5432 -e POSTGRES_DB=postgres -i serv-restore
 
 docker run --name serv -p 5000:5000 -v /Users/AntonEfimenko/pgdata_back:/pgdata_back --link pgs2:db -e POSTGRES_PASSWORD=psw -e PGDATA_BACKUP=/pgdata_back/latest.psql.gz -e BACKUP_RESTORE_LOG=/pgdata_back/restore.log -e POSTGRES_USER=postgres -e POSTGRES_HOST=db -e POSTGRES_PORT=5432 -e POSTGRES_DB=postgres -i serv-restore
+
+docker run --name serv -p 5000:5000 -v /Users/AntonEfimenko/pgdata_back:/pgdata_back --link pgs2:db -e POSTGRES_PASSWORD=psw -e PGDATA_BACKUP=/pgdata_back/latest.psql.gz -e BACKUP_RESTORE_LOG=/pgdata_back/restore.log -e POSTGRES_USER=postgres -e POSTGRES_HOST=db -e POSTGRES_PORT=5432 -e POSTGRES_DB=postgres -e MIGRATE_LOG=/pgdata_back/migrate.log -i reloni/todo-service:dev-latest
