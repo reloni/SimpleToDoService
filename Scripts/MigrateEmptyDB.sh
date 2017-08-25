df20135e8329a20d8b6bc8d25b40d396e055b5d5
Anton Efimenko <reloni@ya.ru>
2017-08-25 08:06:52 +0300

    Add empty DB migration script for tests

---
+++ Scripts/MigrateEmptyDB.sh
@@ -0,0 +1,13 @@
+docker stop migrateempty
+docker run -d --rm --name migrateempty -p 5432:5432 reloni/todo-postgres:latest
+
+export POSTGRES_USER=postgres
+export POSTGRES_PASSWORD=postgres
+export POSTGRES_HOST=localhost
+export POSTGRES_PORT=5432
+export POSTGRES_DB=postgres
+export MIGRATE_LOG=$PWD/migrate.log
+
+sleep 7
+
+sh MigrateDB.sh
